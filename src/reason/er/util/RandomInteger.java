f4716e20f8b04d5142a484f7d4b242e53a280db3
aaronEberhart <aaron.eberhart@gmail.com>
2018-02-15 21:47:32 -0500

    Forgot one tab/space correction.

--- src/reason/er/util/RandomInteger.java
+++ src/reason/er/util/RandomInteger.java
@@ -2,7 +2,7 @@ package reason.er.util;
 
 /**
  * This class was adapted from an example I found online. 
- * Methods that are from the example are labeled  "Original"
+ * Methods that are from the example are labeled	"Original"
  * Methods that I wrote are labeled "New"
  * 
  * 
@@ -11,87 +11,87 @@ package reason.er.util;
  **/
 public class RandomInteger {
 
-	  private long u;
-	  private long v = 4101842887655102017L;
-	  private long w = 1;
+		private long u;
+		private long v = 4101842887655102017L;
+		private long w = 1;
 
-	  /**
-	   * Original
-	   */
-	  public RandomInteger() {
-		  this(System.nanoTime());
-	  }
+		/**
+		 * Original
+		 */
+		public RandomInteger() {
+			this(System.nanoTime());
+		}
 
-	  /**
-	   * Original
-	   * 
-	   * @param seed long
-	   */
-	  public RandomInteger(long seed) {
-	    u = seed ^ v;
-	    nextLong();
-	    v = u;
-	    nextLong();
-	    w = v;
-	    nextLong();
-	  }
+		/**
+		 * Original
+		 * 
+		 * @param seed long
+		 */
+		public RandomInteger(long seed) {
+			u = seed ^ v;
+			nextLong();
+			v = u;
+			nextLong();
+			w = v;
+			nextLong();
+		}
 
-	  /**
-	   * Original
-	   * @return long
-	   */
-	  public long nextLong() {
-	    try {
-	      u = u * 2862933555777941757L + 7046029254386353087L;
-	      v ^= v >>> 17;
-	      v ^= v << 31;
-	      v ^= v >>> 8;
-	      w = 4294957665L * (w & 0xffffffff) + (w >>> 32);
-	      long x = u ^ (u << 21);
-	      x ^= x >>> 35;
-	      x ^= x << 4;
-	      long ret = (x + v) ^ w;
-	      return ret;
-	    } finally {}
-	  }
+		/**
+		 * Original
+		 * @return long
+		 */
+		public long nextLong() {
+			try {
+				u = u * 2862933555777941757L + 7046029254386353087L;
+				v ^= v >>> 17;
+				v ^= v << 31;
+				v ^= v >>> 8;
+				w = 4294957665L * (w & 0xffffffff) + (w >>> 32);
+				long x = u ^ (u << 21);
+				x ^= x >>> 35;
+				x ^= x << 4;
+				long ret = (x + v) ^ w;
+				return ret;
+			} finally {}
+		}
 
-	  /**
-	   * New
-	   * @param bound integer
-	   * @return integer
-	   */
-	  public int nextInt(int bound) {
-		  int val = (int)nextLong();
-		  val = val < 0 ? val *= -1 : val;
-		  return val % bound;
-	  }
-	  
-	  /**
-	   * New
-	   * @return boolean 1:1
-	   */
-	  public boolean nextBoolean() {
-		  return nextInt(2)==0?true:false;
-	  }
-	  
-	  /**
-	   * New
-	   * @param upper integer
-	   * @param lower integer
-	   * @return boolean upper-lower:lower
-	   */
-	  public boolean weightedBool(int upper, int lower) {
-		  return nextInt(upper)>lower?true:false;
-	  }
-	  
-	  /**
-	   * Original
-	   * @param bits integer
-	   * @return integer
-	   */
-	  protected int next(int bits) {
-	    return (int) (nextLong() >>> (64-bits));
-	  }
+		/**
+		 * New
+		 * @param bound integer
+		 * @return integer
+		 */
+		public int nextInt(int bound) {
+			int val = (int)nextLong();
+			val = val < 0 ? val *= -1 : val;
+			return val % bound;
+		}
+		
+		/**
+		 * New
+		 * @return boolean 1:1
+		 */
+		public boolean nextBoolean() {
+			return nextInt(2)==0?true:false;
+		}
+		
+		/**
+		 * New
+		 * @param upper integer
+		 * @param lower integer
+		 * @return boolean upper-lower:lower
+		 */
+		public boolean weightedBool(int upper, int lower) {
+			return nextInt(upper)>lower?true:false;
+		}
+		
+		/**
+		 * Original
+		 * @param bits integer
+		 * @return integer
+		 */
+		protected int next(int bits) {
+			return (int) (nextLong() >>> (64-bits));
+		}
 
 
 }
